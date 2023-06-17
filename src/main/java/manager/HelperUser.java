3b0ffb5bd03e9f7375cac82efe34b819b70a4fea
QAKateF <134771804+QAKateF@users.noreply.github.com>
2023-06-17 19:28:20 +0300

    HW 17.06.2023

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,13 +1,33 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperUser extends HelperBase{
-
 
     public HelperUser(WebDriver wd) {
         super(wd);
     }
 
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//a[.=' Logout ']"));
+    }
+
+    public void logout() {
+        click(By.xpath("//a[.=' Logout ']"));
+    }
+
+    public void openLoginForm() {
+        click(By.cssSelector("a[ng-reflect-router-link='login']"));
+    }
+
+    public void fillLoginForm(String email, String password){
+        type(By.xpath("//input[@id='email']"), email);
+        type(By.xpath("//input[@id='password']"), password);
+    }
+
+    public void submitLoginForm() {
+        click(By.xpath("//button[@type='submit']"));
+    }
 
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,10 +1,23 @@
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
+    @BeforeMethod
+    public void precondition(){
+        if(app.getUser().isLogged()){
+            app.getUser().logout();
+        }
+    }
+
     @Test
     public void loginPositive(){
-
+        app.getUser().openLoginForm();
+        app.getUser().fillLoginForm("qa38@mail.mn","Ghjk1234!");
+        app.getUser().submitLoginForm();
+        app.getUser().pause(1000);
+        Assert.assertTrue(app.getUser().isLogged());
     }
 
 }
